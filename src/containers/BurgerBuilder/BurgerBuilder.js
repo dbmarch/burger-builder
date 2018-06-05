73cbed59f6ee38bd627de11e7a16499e69766ae9
dbmarch <dbmarch@gmail.com>
2018-06-04 20:44:18 -0500

    Static burger built up

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Aux2';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render () {
         return (
             <Aux>
-                <div> Burger </div>
+                <Burger />
                 <div> Burger controls </div>
             </Aux>
         )
