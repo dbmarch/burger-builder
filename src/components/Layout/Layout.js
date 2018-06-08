ee5965b1a0903f7c9786bd7ae565a13a05b69eb6
dbmarch <dbmarch@gmail.com>
2018-06-07 22:13:50 -0500

    responsive side bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux2';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
+import Logo from '../Logo/Logo';
 
 
-const layout = ( props ) => (
-    <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <div> Toolbar, SideDrawer, Backdrop</div>
-    <main className = {classes.Content}>
-        {props.children}
-    </main>
-    </Aux>
 
-);
+class Layout extends Component  {
 
-export default layout;
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+
+        return(
+        <Aux>
+        <Toolbar />
+        <div className = {classes.Logo}>
+                <Logo />
+            </div> 
+        <SideDrawer 
+            open= {this.state.showSideDrawer}
+            closed = {this.sideDrawerClosedHandler} />
+        <main className = {classes.Content}>
+            {this.props.children}
+        </main>
+        </Aux>
+        );
+    }
+}
+export default Layout;
