1ab0ac526fca4f14fd3a92079773ecd5a9796f70
dbmarch <dbmarch@gmail.com>
2018-06-07 22:14:34 -0500

    forgot a file

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -15,8 +15,8 @@ const sideDrawer = (props) => {
         <Aux>
         <Backdrop show={props.open} clicked={props.closed}/>
         <div className = {attachedClasses.join[' ']}>
-            <div className = {classes.Logo2}>
-                    <Logo />
+            <div className = {classes.Logo}>
+                <Logo />
             </div> 
 
             <nav>
